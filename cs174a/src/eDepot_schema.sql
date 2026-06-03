250fa3f3dbabc22bb8e2ef0e3dcb0693e1673560
Keigo Omura <komura@ucsb.edu>
2026-06-02 17:24:23 -0700

    finishing eDEPOT schema

--- cs174a/src/eDepot_schema.sql
+++ cs174a/src/eDepot_schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Inventory (
-    stock_number VARCHAR2(50) PRIMARY KEY,
+    stock_number VARCHAR2(7) PRIMARY KEY,
     manufacturer VARCHAR2(100) NOT NULL,
     model_number VARCHAR2(100) NOT NULL,
     quantity INTEGER NOT NULL,
@@ -8,15 +8,57 @@ CREATE TABLE Inventory (
     location VARCHAR2(25) NOT NULL,
     replenishment INTEGER,
 
-    CONSTRAINT check_quantity
-        CHECK (quantity > 0 AND quantity < max_stock_level),
+    CONSTRAINT unique_manufacturer_model 
+        UNIQUE (manufacturer, model_number),
+
+    CONSTRAINT check_inventory_quantity
+        CHECK (quantity >= 0 AND quantity < max_stock_level),
 
     CONSTRAINT check_min_stock
-        CHECK (min_stock_level > 0),
+        CHECK (min_stock_level >= 0),
 
     CONSTRAINT check_max_stock
-        CHECK (max_stock_level > 0 AND max_stock_level > min_stock_level),
+        CHECK (max_stock_level > min_stock_level),
 
     CONSTRAINT check_replenishment
-        CHECK (replenishment > 0)
+        CHECK (replenishment >= 0),
+
+    CONSTRAINT check_stock_number
+        CHECK (REGEXP_LIKE(stock_number, '^[A-Z]{2}[0-9]{5}$')),
+    
+    CONSTRAINT ck_location          
+        CHECK (REGEXP_LIKE(location, '^[A-Za-z][1-9][0-9]*$'))
+);
+
+CREATE TABLE ShippingNotice (
+    notice_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    shipping_company VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE ShippingNoticeItem (
+    notice_id INTEGER NOT NULL,
+    manufacturer VARCHAR2(100) NOT NULL,
+    model_number VARCHAR2(100) NOT NULL,
+    quantity INTEGER NOT NULL,
+
+    PRIMARY KEY (notice_id, manufacturer, model_number),
+    FOREIGN KEY (notice_id) REFERENCES ShippingNotice(notice_id),
+    CONSTRAINT check_shipping_quantity  
+        CHECK (quantity > 0)
+);
+
+CREATE TABLE ReplenishmentOrder (
+    replenishment_order_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    manufacturer VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE ReplenishmentOrderItem (
+    replenishment_order_id INTEGER NOT NULL,
+    stock_number VARCHAR2(7) NOT NULL,
+    quantity_requested INTEGER NOT NULL,
+
+    PRIMARY KEY (replenishment_order_id, stock_number),
+    FOREIGN KEY (replenishment_order_id) REFERENCES ReplenishmentOrder(replenishment_order_id),
+    FOREIGN KEY (stock_number) REFERENCES Inventory(stock_number),
+    CONSTRAINT check_replenishment_quantity CHECK (quantity_requested > 0)
 );
